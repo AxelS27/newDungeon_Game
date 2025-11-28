f19b7bf36d2200a107051efc71766d4d2655199c
AxelS27 <moskop25@gmail.com>
2025-11-28 15:30:38 +0700

    Part 5

--- src/main/java/com/jawa/entity/Player.java
+++ src/main/java/com/jawa/entity/Player.java
@@ -12,17 +12,23 @@ public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
+
+        screenX = gp.screenWidth/2 - (gp.tileSize/2);
+        screenY = gp.screenHeight/2 - (gp.tileSize/2);
 
         setDefaultValues();
         getPlayerImage();
     }
 
     public void setDefaultValues(){
-        x = 100;
-        y = 100;
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
         speed = 4;
         direction = "down";
     }
@@ -46,19 +52,19 @@ public class Player extends Entity {
         if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed){
             if (keyH.upPressed){
                 direction = "up";
-                y -= speed;
+                worldY -= speed;
             }
             else if (keyH.downPressed){
                 direction = "down";
-                y += speed;
+                worldY += speed;
             }
             else if (keyH.leftPressed){
                 direction = "left";
-                x -= speed;
+                worldX -= speed;
             }
             else if (keyH.rightPressed){
                 direction = "right";
-                x += speed;
+                worldX += speed;
             }
             spriteCounter++;
             if (spriteCounter > 12){
@@ -73,45 +79,42 @@ public class Player extends Entity {
 
     }
 
-    public void draw(Graphics2D g2){
-//        g2.setColor(Color.white);
-//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
-
+    public void draw(Graphics2D g2) {
         BufferedImage image = null;
-        switch(direction){
+        switch (direction) {
             case "up":
-                if (spriteNum == 1){
+                if (spriteNum == 1) {
                     image = up1;
                 }
-                if (spriteNum == 2){
+                if (spriteNum == 2) {
                     image = up2;
                 }
                 break;
             case "down":
-                if (spriteNum == 1){
+                if (spriteNum == 1) {
                     image = down1;
                 }
-                if (spriteNum == 2){
+                if (spriteNum == 2) {
                     image = down2;
                 }
                 break;
             case "left":
-                if (spriteNum == 1){
+                if (spriteNum == 1) {
                     image = left1;
                 }
-                if (spriteNum == 2){
+                if (spriteNum == 2) {
                     image = left2;
                 }
                 break;
             case "right":
-                if (spriteNum == 1){
+                if (spriteNum == 1) {
                     image = right1;
                 }
-                if (spriteNum == 2){
+                if (spriteNum == 2) {
                     image = right2;
                 }
                 break;
         }
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 }
--- src/main/java/com/jawa/tile/Tile.java
+++ src/main/java/com/jawa/tile/Tile.java
@@ -1,10 +1,19 @@
 package com.jawa.tile;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Tile {
 
     public BufferedImage image;
     public boolean collision = false;
+    public Tile(String filePath) {
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream(filePath));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
 }

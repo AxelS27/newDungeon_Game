900366ead4f76e5ee29d30cc4a437ca1a602ba2a
AxelS27 <moskop25@gmail.com>
2025-12-14 19:22:34 +0700

    The Path Of Winter

--- src/main/java/com/TPOW/main/Main.java
+++ src/main/java/com/TPOW/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Dungeon Game");
+        window.setTitle("The Path Of Winter");
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
         window.pack();
